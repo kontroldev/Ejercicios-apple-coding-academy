40f2ba4ca53342b41d8058f4ca39e4a4cc297e3a
Kontrol dev <raulgallego79@icloud.com>
2023-05-02 17:28:51 +0200

    Actualizacion y corrección de algunas clases con contenían errores

--- Applecoding.playground/Pages/3.6.3 Añadiendo - quitando elementos a una Array.xcplaygroundpage/Contents.swift
+++ Applecoding.playground/Pages/3.6.3 Añadiendo - quitando elementos a una Array.xcplaygroundpage/Contents.swift
@@ -2,8 +2,7 @@ import Foundation
 
 var actores:[String] = []
 
-
-actores.append("Will Smith") //Añadiendo un elemento
+actores.append("Will Smith") //Añadiendo un elemento a un Array
 actores += ["Fresh Prince", "El principe de Bel-air", "Tia Bibian", "Hilary", "Ashley Banks"] //Añadiendo varios elementos a la vez. Utilizamos el Operador de agregacion.
 print(actores)
 
@@ -15,6 +14,7 @@ print(actores)
 
 //AÑADIENDO VALORES MEDIANTE RANGOS DE POSICIONES y CAMBIANDO SUS VALORES
 actores[1...3] = ["El Pincipe", "Carton Banks", "Tio Fil"]
+//Si no le damos los suficientes datos,(Ejemplo: si no ponemos el ultimo valor) eliminara al valor que ya tenia anterioemente y si le ponemos un valor de mas al rango, inserta en la posicion posteiror al rango que le hemos dicho
 print(actores)
 
 
@@ -26,6 +26,7 @@ actores.insert(contentsOf: ["Freddy", "Bibian"],at: 3)// Al insertar mas element
 print(actores)
 
 
+
 //QUITANDO ELEMENTO DE UNA ARRAY
 actores.dropFirst(3)//Me quita los 3 primeros y me hace una copia de los anterior elementos.
 print(actores)
@@ -34,13 +35,42 @@ actores.removeLast() //Elimina el ultimo elemento.
 actores.removeLast(2) //Elimina los 3 ultimos.
 actores.removeFirst() //Me elimina el primero primeros
 
+/*
 actores.remove(at: 3) //Me borra una posicion en concreto.
-actores.removeSubrange(2...4) //Borra con opoerador de Rango
+actores.removeSubrange (2...4) //Borra con opoerador de Rango
 actores.removeAll() //Me elimina todo los datos de la Array
 actores.removeAll(keepingCapacity: true) // NO se borra los valores que estan en memoria para poder utilizarlos mas tarde.
 
 
-//RANGO EN UN ARRAY DE UN SOLO LADO
+RANGO EN UN ARRAY DE UN SOLO LADO
 actores[..<4]
 actores[4...]
+*/
+
+
+//EXTRA ⬇️ Clase añadida.
+
+var compositores = ["John Williams","Danny Elfman", "James Newton Howard", "Brian Tyler","Michael Giacchino", "Alexandre Desplat", "John Powell", "Howard Shore", "Harry Gregson-Williams", "Henry Jackman", "James Horner"]
+
+// Podemos extraer el primer o el último valor, pero conseguimos una copia donde se ha quitado el valor, no lo quita al que tenemos
+compositores.dropFirst()
+compositores.dropLast()
+
+// Podemos borrar en una posición
+compositores.remove(at: 5)
+
+// El borrado extrae el dato realmente y lo devuelve como resultado
+let compositor = compositores.remove(at: 5)
+
+// Podemos borrar un rango
+compositores.removeSubrange(2...4)
+
+// Podemos borrar el último y lo devuelve como resultado
+compositores.removeLast()
+
+// Podemos borrar todos
+compositores.removeAll()
+
+// O podemos borrar todos pero conservando la capacidad del array (Una carga temporal)
+compositores.removeAll(keepingCapacity: true)
 
--- Applecoding.playground/Pages/3.6.5 Buscando y enumerendo.xcplaygroundpage/Contents.swift
+++ Applecoding.playground/Pages/3.6.5 Buscando y enumerendo.xcplaygroundpage/Contents.swift
@@ -1,5 +1,6 @@
 import Foundation
 
+
 var compositores = [String]()
 compositores.append("John Williams")
 compositores += ["Danny Elfman", "James Newton Howard", "Brian Tyler"]
@@ -12,18 +13,22 @@ compositores.insert("Howard Shore", at: 4)
 compositores.insert(contentsOf: ["Harry Gregson-Williams", "Henry Jackman", "James Horner"], at: 3)
 
 
+
+
 //RANGOS DE UN SOLO LADO CON LOS ARRAYS
-compositores[...4] //Me devuelve un ARRAY de un solo lado y me devuelve los primeros 5 primeros elementos.
-compositores[4...] //Me devuelve los valores de un ARRAY desde la 4ª posicion hasta el final.
-compositores[..<2] = ["Joel McNeely", "Bruce Broughton"] //Sustituye los dos primeros elementos de la ARRAY.
+compositores[..<4]  //Me deveuleve de un ARRAY los 5 primeros de un solo lado
+compositores[...4]  //Me deveuleve de un ARRAY los 4 primeros de un solo lado
+compositores[4...]  //Me devuelve los valores de un ARRAY desde la 4ª posicion hasta el final.
+compositores[..<2] = ["Joel McNeely", "Bruce Broughton"]  //Sustituye los dos primeros elementos de la ARRAY.
 compositores
 
+
 //BUSQUEDA POR LA PRIMERA O LA ULTIMA OCURRENCIA
-compositores.firstIndex(of: "Hans Zimmer") //Esto nos dice El valor que estamos buscando en que posicion esta en una ARRAY.
+compositores.firstIndex(of: "Hans Zimmer") //Esto nos dice el valor que estamos buscando y en que posicion esta de un ARRAY.
 
 let numeros = [1,6,4,7,8,5,7,3,8,6]
-numeros.lastIndex(of: 7) // Me busca un valor desde el final.
-numeros.firstIndex(of: 7) // Me busca un valor desde el principal.
+numeros.lastIndex(of: 7) // Me busca un valor desde el final de un ARRAY.
+numeros.firstIndex(of: 7) // Me busca un valor desde el principal de un ARRAY.
 
 
 //ENUMERACION FOR IN
@@ -38,7 +43,7 @@ for valor in compositores.enumerated(){ //Valor del indice, que corresponde del 
 for (indice, compositor) in compositores.enumerated() {
         print("Posicion \(indice) es \(compositor)")
 }
-/* ⬆️Este código utiliza un ciclo for-in para recorrer un array de elementos compositores y obtener su índice y valor correspondiente utilizando la función enumerated().
+/* ⬆️ Este código utiliza un ciclo for-in para recorrer un array de elementos compositores y obtener su índice y valor correspondiente utilizando la función enumerated().
  
  En la primera línea del ciclo for-in, se define una tupla (indice, compositor) que se utiliza para almacenar el índice y valor del elemento actual.
 
