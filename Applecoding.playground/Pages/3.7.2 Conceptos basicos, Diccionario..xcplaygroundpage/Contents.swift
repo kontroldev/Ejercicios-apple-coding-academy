abf50dd1d80d6431dc1664ce9ec5b4d2cc1e7882
kontroldev <raulgallego79@icloud.com>
2023-12-24 20:04:37 +0100

    Actualización y añadir clases - 24/12/2023

--- Applecoding.playground/Pages/3.7.2 Conceptos basicos, Diccionario..xcplaygroundpage/Contents.swift
+++ Applecoding.playground/Pages/3.7.2 Conceptos basicos, Diccionario..xcplaygroundpage/Contents.swift
@@ -11,47 +11,29 @@ import Foundation
  Y un diccionario es una coleccion y podemos iterar sobre ella
  los valores que puedes almacenar en un diccionario son: 'Int' 'String' 'Float/Double' 'Bool'*/
 
+// Siempre los diccionarios deben de ser del mismo tipo, no podemos poner un diccionario de 'Int:String junto con un Bool o un Double.
 
 // ARRAY VACIA
 var array:[String] = [] // Un ARRAY vacia de tipo 'String'
 
 
 // DECLARACION DE UN DICCIONARIO VACIO
-var diccionario:[Int:String] = [:] // Inicializamos un dicionario vacio, que tiene una clave 'Int' y valor 'String' y la inicializamos vacia con dos puntos dentro de los corchetes.
+var diccionario:[Int:String] = [:] // Inicializamos un dicionario vacio, que tiene una clave 'Int' y valor 'String' y la inicializamos vacia con dos puntos dentro de los corchetes
 
 
 // AÑADIR DATOS A UN DICCIONARIO
-diccionario = [1:"Uno", 2:"Dos", 3:"Tres"]  // sin nos fijamos de como nos lo entrega -> veremos que no estan ordenados tal y como se lo hemos puesto, por que un diccionario nunca sera ordenado, y accedemos a esos valores mediante la clave, en este caso sera un 'Int'
+diccionario = [1:"Uno", 2:"Dos", 3:"Tres"]  // sin nos fijamos de como nos lo entrega "infiere" -> veremos que no estan ordenados tal y como se lo hemos puesto, por que un diccionario nunca sera ordenado, y accedemos a esos valores mediante la clave, en este caso sera un 'Int'
+
 
 var diccionario2:[Int:Int] = [1:5, 2:3, 6:7, 8:1]
-// NOS QUEDAMOS AQUI
+
 
 // INFERENCIA DE DICCIONARIOS
 var diccionario3 = [5:"Cinco", 6:"Seis", 7:"Siete"] // No es necesario definir este diccionario, por que el mismo va inferir, que clave : valor son.
  
 
-// DEFINICION PRIMITIVA O GENERICO
-var diccionario4 = Dictionary<String, Int>() // Es mejor hacerlo de las otras opciones como (var diccionario:[Int:String] = [:]) ya que esta es una forma mas generica.
+// DEFINICION PRIMITIVA O GENERICO/LEGACY
+var diccionario4 = Dictionary<String, Int>() // Es mejor hacerlo de las otras opciones como (var diccionario:[Int:String] = [:]) ya que esta es una forma mas generica o LEGACY
 diccionario4 = ["Uno":1, "Dos":2, "Tres":3]
-
-
-// ACCESO POR LA CLAVE
-diccionario2[6] // POner entre corchetes la clave y recibiremos el valor. Mirar ejemplo ⬆️
-diccionario3[7]
-if let valor = diccionario4["Dos"] {  // Cualquier valor que recuperemos de un diccionario, es un valor opcional siempre.
-    print(valor)                   // El uso del if let, indica que se realizará una comprobación de nulidad para asegurarse de que el valor exista y no sea nulo.
-}
-
-// PREGUNTAR POR UN VALOR
-if diccionario4["Dos"] == 2 {
-    
-}                                       // Se puede hacer de las dos maneras.
-
-if let valor = diccionario4["Dos"], valor == 2 {
-    
-}
-
-
-diccionario2[50]   // Cuidado a la hora de trabajar con los diccionarios en este sentido, por que si querenmos acceder a un valor que no existe. por que sabemos que no va a ver problema a ser un diccioanrio, pero hay que tenerlo en cuenta.
 
 
---
+++ Applecoding.playground/Pages/3.7.3 Accediendo a los datos  DICCIONARIO.xcplaygroundpage/Contents.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+var array:[String] = []
+var diccionario:[Int:String] = [:]
+diccionario = [1:"Uno", 2:"Dos", 3:"Tres"]
+var diccionario2:[Int:Int] = [1:5, 2:3, 6:7, 8:1]
+var diccionario3 = [5:"Cinco", 6:"seis", 7:"Siete"]
+var diccionario4 = Dictionary<String, Int>()
+diccionario4 = ["Uno":1, "Dos":2, "Tres":3]
+
+
+// ACCESO POR LA CLAVE
+diccionario2[6] // Poner entre corchetes la clave y recibiremos el valor. Mirar ejemplo ⬆️
+diccionario3[7]
+diccionario4["Dos"] // aqui accedemos al diccionario creados posteriormente y le hemos cambiado los valores, antes eran [String:Int] y ahora es [Int:String]  (mira arriba 👆)
+
+// cualquier valor que recuperemos de un diccionario al igual que podemos hacer en un ARRAY, en los array no tenemos control de errores y en un diccionario si accedemos a un valor que no existe, vamos a obtener un valor opcional siempre.
+// Y para acceder a un diccionario opcional  ME QUEDE AQUI Y ALADIR COMENTARIOS
+
+if let valor = diccionario4["Dos"] {  // Cualquier valor que recuperemos de un diccionario, es un valor opcional siempre.
+    print(valor)                   // El uso del if let, indica que se realizará una comprobación de nulidad para asegurarse de que el valor exista y no sea nulo.
+}
+
+// PREGUNTAR POR UN VALOR
+if diccionario4["Dos"] == 2 {
+    
+}                                       // Se puede hacer de las dos maneras.
+
+if let valor = diccionario4["Dos"], valor == 2 {
+    
+}
+
+
+diccionario2[50]   // Cuidado a la hora de trabajar con los diccionarios en este sentido, por que si querenmos acceder a un valor que no existe. por que sabemos que no va a ver problema a ser un diccioanrio, pero hay que tenerlo en cuenta.
+
+
+
+
+
+// DATO OPCIONAL
+// PREGUNTANDO UN VALOR
+// NIL
