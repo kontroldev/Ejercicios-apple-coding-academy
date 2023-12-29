86acdf4f2fd2018bb2305796ce557d2cf6d75f31
kontroldev <raulgallego79@icloud.com>
2023-12-29 18:45:49 +0100

    Actualización clases 29/12/2023

--- Applecoding.playground/Pages/3.8.2 Inicializando los SET.xcplaygroundpage/Contents.swift
+++ Applecoding.playground/Pages/3.8.2 Inicializando los SET.xcplaygroundpage/Contents.swift
@@ -1,24 +1,24 @@
 import Foundation
 
+
+/* Es una coleccion no ordenada, no idexada de elementos unicos que no se pueden repetir*/
+
 // CREANDO UN SET
-/* Es una coleccion no ordenada, no idexada de elementos unicos y no se pueden repetir*/
+var conjunto1 = Set<String>() // esto es un 'SET' de 'String' vacio
+var conjunto2:Set = ["Jonh Williams", "James Horner", "Jerry Goldsmith"]  // Aqui inferimos conjunto2 explicitamente como un SET con ':Set' de lo contrario el sistema me lo infiere como una array.
 
 
-var conjunto1 = Set<String>()
-var conjunto2:Set = ["Jonh Williams", "James Horner", "Jerry Goldsmith"]  // Aqui inferimos conjunto2 explicitamente como un conjunto SET
-
-
-//CONTANDO ELEMENTOS DE UN CONJUNTO(SET)
+//CONTANDO ELEMENTOS DE UN SET
 conjunto1.count
 conjunto2.count
 
 
 //COMPROBAR LA EXISTENCIA DE UN VALOR
-conjunto2.contains("James Horner")
+conjunto2.contains("James Horner") // aqui preguntamos si contiene un valor en concreto, "James Horner" y nos devuelve un Bool
 
 
 //ACCEDER AL PRIMER ELEMENTO
-conjunto1.first
+conjunto1.first  // accedemos al primer valor del Set.
 conjunto2.first
 
 
@@ -26,16 +26,17 @@ conjunto2.first
 conjunto2.insert("Michael Kamen")
 conjunto2.insert("David Shire")   // Nos devuelve TRUE, por que a añadido estos valores al conjunto.
 
-conjunto2.insert("James Horner") // Nos devuelve un FALSE, por que este valor ya esta dentro del SET(conjunto), y no lo a insertado.
-conjunto2.insert("James Horner").inserted // Como controlar si hay un dato en un SET.
+conjunto2.insert("James Horner") // Nos devuelve un FALSE, por que este valor ya esta dentro del SET, y no lo a insertado.
+conjunto2.insert("James Horner").inserted  // Como controlar si hay un dato en un SET, con interfaces fluidas, que seria '.inserted'
 
 if conjunto2.insert("Michael Jordan").inserted { // Con esto tambien insertamos un valor y nos dice que el valor esta insertado.
     print("Valor insertado")
 }
 
+
 //BORRANDO DATOS
-conjunto2.remove("David Shire")
-conjunto2.remove("David Shire") // Y aqui devuelve nill, por que es un valor que ya no esta en SET.
+conjunto2.remove("David Shire")  // aqui eliminamos el valor del Set - tambien hay varias opciones: '.removeAll' '.removeFirst'
+conjunto2.remove("David Shire") // Y aqui devuelve nill, por que es un valor que ya no esta en SET, por que lo hemos borrado anteriormente.
 
 
 //ITERANDO
