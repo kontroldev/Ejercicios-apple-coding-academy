b0168b10068ac91b4452cfc5942ee5ca6e94ceae
kontroldev <raulgallego79@icloud.com>
2023-12-30 19:12:17 +0100

    Actualización de clases - 30/12/2023

--- Applecoding.playground/Pages/3.8.2 Inicializando los SET.xcplaygroundpage/Contents.swift
+++ Applecoding.playground/Pages/3.8.2 Inicializando los SET.xcplaygroundpage/Contents.swift
@@ -5,7 +5,7 @@ import Foundation
 
 // CREANDO UN SET
 var conjunto1 = Set<String>() // esto es un 'SET' de 'String' vacio
-var conjunto2:Set = ["Jonh Williams", "James Horner", "Jerry Goldsmith"]  // Aqui inferimos conjunto2 explicitamente como un SET con ':Set' de lo contrario el sistema me lo infiere como una array.
+var conjunto2:Set = ["Jonh Williams", "James Horner", "Jerry Goldsmith"]  // Aqui inferimos conjunto2 explicitamente como un SET con ':Set' de lo contrario el sistema me lo infiere como una array de tipo String.
 
 
 //CONTANDO ELEMENTOS DE UN SET
@@ -14,22 +14,23 @@ conjunto2.count
 
 
 //COMPROBAR LA EXISTENCIA DE UN VALOR
-conjunto2.contains("James Horner") // aqui preguntamos si contiene un valor en concreto, "James Horner" y nos devuelve un Bool
+conjunto2.contains("James Horner") // aqui preguntamos si contiene un valor en concreto, "James Horner" y nos devuelve un Bool.
 
 
 //ACCEDER AL PRIMER ELEMENTO
-conjunto1.first  // accedemos al primer valor del Set.
+conjunto1.first  // accedemos al primer valor del Set. - ‼️ no hay un '.last' con los Set.
 conjunto2.first
 
 
 //INSERTAR VALORES EN UN SET
 conjunto2.insert("Michael Kamen")
 conjunto2.insert("David Shire")   // Nos devuelve TRUE, por que a añadido estos valores al conjunto.
+print(conjunto2)
 
 conjunto2.insert("James Horner") // Nos devuelve un FALSE, por que este valor ya esta dentro del SET, y no lo a insertado.
 conjunto2.insert("James Horner").inserted  // Como controlar si hay un dato en un SET, con interfaces fluidas, que seria '.inserted'
 
-if conjunto2.insert("Michael Jordan").inserted { // Con esto tambien insertamos un valor y nos dice que el valor esta insertado.
+if conjunto2.insert("Michael Jordan").inserted { // Con esto tambien insertamos un valor y  el '.inserted' nos dice que el valor se a insertado.
     print("Valor insertado")
 }
 
@@ -41,7 +42,8 @@ conjunto2.remove("David Shire") // Y aqui devuelve nill, por que es un valor que
 
 //ITERANDO
 for valor in conjunto2 {
-    print(valor)    // Y los imprime no ordenados cada vez que lo ejecutamos.
+    print(valor)    // Y los imprime no ordenados cada vez que lo ejecutamos. (mirar en consola ⬇️)
 }
-for valor in conjunto2.sorted() {  // Y aqui utilizando `.sorted` nos ordenara en forma alfabeticamente.
+
+for valor in conjunto2.sorted() {  // Y aqui utilizando '.sorted' nos ordenara en forma alfabeticamente. ⬇️
     print(valor)}
