4f249b09990c91bb5334385ca02731e307f1844e
kontroldev <raulgallego79@icloud.com>
2024-01-10 21:16:10 +0100

    Actualización de clases

--- Applecoding.playground/Pages/3.10.1 Tuplas, conceptos.xcplaygroundpage/Contents.swift
+++ Applecoding.playground/Pages/3.10.1 Tuplas, conceptos.xcplaygroundpage/Contents.swift
@@ -17,16 +17,4 @@ pelicula.pelicula = "Inside Out"
 
 print(pelicula)
 
-//: 3.10.2 Descomponiendo una Tupla
-// ASIGNANDO VALORES POR DESCOMPOSICION.
-let (film, year, rank) = pelicula
-
-film
-year
-rank
-
-let nombre = pelicula.pelicula
-
-
-//PLACEHOLDERS PARA RECHAZAR DATOS.
-let (nombre2, _, _) = pelicula  // Las barras bajas seria un placeholders o un espacio reservado.
+//: 
---
+++ Applecoding.playground/Pages/3.10.2 Descomponiendo una Tupla.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+// ASIGNANDO VALORES POR DESCOMPOSICION.
+let (film, year, rank) = pelicula
+
+film
+year
+rank
+
+let nombre = pelicula.pelicula
+
+
+//PLACEHOLDERS PARA RECHAZAR DATOS.
+let (nombre2, _, _) = pelicula  // Las barras bajas seria un placeholders o un espacio reservado.
+
