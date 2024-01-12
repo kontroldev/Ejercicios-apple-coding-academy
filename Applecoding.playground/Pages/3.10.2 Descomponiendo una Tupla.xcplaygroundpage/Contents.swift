28aea96f36e924d1ce7fd54830f05d306746af7a
kontroldev <raulgallego79@icloud.com>
2024-01-12 13:44:49 +0100

    Actualización de clases.

--- Applecoding.playground/Pages/3.10.2 Descomponiendo una Tupla.xcplaygroundpage/Contents.swift
+++ Applecoding.playground/Pages/3.10.2 Descomponiendo una Tupla.xcplaygroundpage/Contents.swift
@@ -1,15 +1,22 @@
 import Foundation
 
+var pelicula = (pelicula: "Tomorrowland", año: 2015, rank: 8.4)
+pelicula.pelicula = "Inside Out"
+
 // ASIGNANDO VALORES POR DESCOMPOSICION.
-let (film, year, rank) = pelicula
+let (film, year, rank) = pelicula  // de esta manera descomponemos la tupla, y podemos extraer todo los datos de la misma y asi no sera necesario llamar a la tupla junto con el punto. etc etc y creamos 3 constentes nuevas, que cada uno tendra un valor de su valor, por que anteriormnete quedaron definidas.
 
-film
-year
-rank
+film  // tiene un valor de: '"Inside Out"' ➡️
+year  // tiene un valor de: '2015' ➡️
+rank  // tiene un valor de: '8,4' ➡️
 
-let nombre = pelicula.pelicula
+let nombre = pelicula.pelicula // y esto 'nombre' optiene el valor de 'Inside Out'
 
 
-//PLACEHOLDERS PARA RECHAZAR DATOS.
+//PLACEHOLDERS
+/* Un "placeholder" en programación, se refiere a un marcador o espacio reservado que indica dónde se debe colocar un dato o contenido específico en tiempo de ejecución. Los placeholders son utilizados para representar información que será reemplazada por valores reales cuando se ejecute el programa.*/
+
 let (nombre2, _, _) = pelicula  // Las barras bajas seria un placeholders o un espacio reservado.
+// esta funcion es muy parecida a la de arriba, que crea una nueva constante 'nombre2' y le asigna el valor de 'pelicula - "Inside Out"' por que tiene dos 'placeholder'
 
+nombre2
--- Applecoding.playground/Pages/3.10.3 Arrays de tuplas.xcplaygroundpage/Contents.swift
+++ Applecoding.playground/Pages/3.10.3 Arrays de tuplas.xcplaygroundpage/Contents.swift
@@ -1,28 +1,44 @@
 import Foundation
 
+
+// Si queres saber que es un 'Array' - 3.6.2
 // ETIQUETAS EN LAS TUPLAS EN UN ARRAY
 let películas:[(película:String, año:Int, rank:Double)] = [("Tomorrowland", 2015, 9.0), ("Inside Out", 2015, 9.7), ("Interstellar", 2014, 10), ("Groundhog Day", 1993, 8.0), ("The Goonies", 1985, 8.8), ("The Dark Knight", 2008, 9.1), ("Artificial Intelligence", 2001, 9.8), ("Jurassic Park", 1993, 7.7)]
-// Pelicula: String etc etc... sopn la etiquetas de una tupla.
+// Pelicula: 'String', 'Int', 'Double' - son la etiquetas de una tupla.
+
 
 // ENUMERANDO UN ARRAY DE TUPLAS
 for film in películas {
     print("La pelicula \(film.película) se estreno el año \(film.año) y tiene una valoracion de \(film.rank).")
 }
+// ⬆️ aqui mediante un bucle 'for' llamado film, coge los datos de pelicula y luego la impimimos descomponiendo los datos que lleva la tupla, 'interpolando' los valores de cada uno de los datos que tiene cada tupla.
+
 
 for film in películas where film.rank >= 9 {  // Aqui me va sacar las peliculas qur tiene un valoracion de ranking de un 9.
     print("La pelicula \(film.película) se estreno el año \(film.año) y tiene una valoracion de \(film.rank).")
 }
+// ⬆️ y en este bucle/funcion valoreme solo las peliculas que tengas un '9' de valoracion
 
 
 // SWITCH DE UNA TUPLA EN UN ARRAY
 for film in películas {
     var mensaje:String
-    switch film {   //  ↙️ Aqui valora que la pelicula es de 2015. Solo entra en el `case`las peliculas que son del 2015.
-    case (_, 2015, _): mensaje = "La pelicula \(film.película) es de 2015"
-    case let (_, year, _) where year < 2000: mensaje = "La pelicula \(film.película) es anterior al año 2000"  // Aqui sacamos solo el año de las peliculas anterior al año 2000.
+    switch film {
+    case (_, 2015, _): mensaje = "La película \(film.película) es de 2015"
     default: mensaje = ""
     }
-    if !mensaje.isEmpty {  // Esto me quita los saltos de linea.
+    print(mensaje)
+}
+// ⬆️en este bucle conseguimos que solo entran en el 'case' las peliculas que son del año 2015 y se imprima con la interpolacion y en el 'case' es como si tuvieramos un = de que todas las peliculas del año 2015 imprima luego el texto, pero es mejor hacerlo con el 'case' por que asi vamos a un valor en concreto.
+
+for film in películas {
+    var mensaje:String
+    switch film {
+    case (_, 2015, _): mensaje = "La pelicula \(film.película) es de 2015"
+    case let (_, year, _) where year < 2000: mensaje = "La pelicula \(film.película) es anterior al año 2000"  // Aqui sacamos solo el año de las peliculas anterior al año 2000 - donde el año sea menor del año 2000.
+    default: mensaje = ""
+    }
+    if !mensaje.isEmpty {  // si el mesnaje no esta vacio, que me lo imprima.
         print(mensaje)
     }
 }
