53faf37f06b1d3473f1dba5f23bd771c0a74c37a
Raul Gallego <raulgallego79@icloud.com>
2024-10-29 00:39:02 +0100

    Actualizacion de clases 3.3.6

--- Swift - Basico.playground/Pages/3.3.5 Bucles While y repeat while.xcplaygroundpage/Contents.swift
+++ Swift - Basico.playground/Pages/3.3.5 Bucles While y repeat while.xcplaygroundpage/Contents.swift
@@ -3,24 +3,26 @@ import Foundation
 
 /// **BUCLE REPEAT WHILE
 var valor:Int
+
 let acierto = Int.random(in: 1...20)   // acierto es igual a un numero aleatorio entre 1 y 20
 print("Buscando el numero \(acierto)")
 
 repeat {                  // bucle de salida
-    valor = Int.random(in: 1...20)
+    valor = Int.random(in: 1...20) // Inicializamos valor dentro del ambito del repeat.
     print("Ha salido el valor \(valor)")
     } while acierto != valor   // mientras que esta condicion se este cumpliendo 'true', repeat se estara cumpliendo hasta que sea 'false'.
-    // 👆 la condicion de salida del bucle seria aqui al final con el repeat-while
+    // 👆 Este tiene la condicion de salida del bucle al final con el repeat-while.
 
 
 /// **BUCLE WHILE
-var valor2 = -1
+var valor2 = -1 // Le ponemos este valor para al menos garantizarnos que entraria una vez en el bucle.
 let acierto2 = Int.random(in: 1...20)
 print("Buscando el \(acierto2)")
 
+// 👇  En este bucle, la condicion la tiene al principio.
 while acierto2 != valor2 {  // de entrada
     valor2 = Int.random(in: 1...20)
-    //valor2 = 22  <- VIGILAR en poner los valores por nos puede dar bucles infinitos, y ponemos -1 para asi entra 1 vez en el bucle.
+    //valor2 = 22  <- ⚠️VIGILAR en poner los valores, por que nos puede dar bucles infinitos.
     print("Ha salido el valor \(valor2)")
 }
 
--- Swift - Basico.playground/Pages/3.3.6 Control de transferencia y etiquetas.xcplaygroundpage/Contents.swift
+++ Swift - Basico.playground/Pages/3.3.6 Control de transferencia y etiquetas.xcplaygroundpage/Contents.swift
@@ -1,24 +1,29 @@
 import Foundation
 
+
 /// **BREAK
 let acierto = (Int(arc4random()) % 20) + 1
 print ("Buscando el numero \(acierto)")
+
 for indice in 1...20{
     if acierto != indice {
         print ("Buscando el numero \(acierto). Voy a por el \(indice).")
-    } else {   // se crea un else para poner la funcion break, para que una vez que a encontrado el numero, rompemos el flujo del for in, de no estar el else, llegaria al 20.
-        break //⬅️ Esto romper el fujo con BREAK
+    } else {
+        break //⬅️ Esto romper el fujo, 'break'.
     }
 }
+// 👆se crea un else para poner la funcion break, para que una vez que a encontrado el numero, rompemos el flujo del for in.
 
 
 /// **CONTINUE
 let dato1 =  Int(arc4random() % 200)
-let dato2 =  Int(arc4random() % 200) 
+let dato2 =  Int(arc4random() % 200)
 let dato3 =  Int(arc4random() % 200)
 
+
+/* 👇Esta condicion, si no es igual a ninguno de los 3 condiciones que estamos evaluando, haga 'continue' y vuelve al bucle for otra vez (hacia arriba en vez de romper como hacia con el break, y de esta manera no entra en el 'switch' 200 veces a pesar solo necesitamos 3 posibles casos)*/
 for i in 0...199 {
-    if i != dato1 && i != dato2 && i != dato3 {  /* esta condicion, si no es igual a ninguno de los 3 condiciones que estamos buscando, haga continue y vuelve al bucle for otra vez (hacia arriba en vez de romper como hacia con el break, y de esta manera entra en el switch 200 veces a pesar solo necesitamos 3 posibles casos)*/
+    if i != dato1 && i != dato2 && i != dato3 { // Se tienen que cumplir al menos 1 valor.
         continue
     }
     switch i {
@@ -32,14 +37,37 @@ for i in 0...199 {
 
 /// **FALLTHROUGH
 let x = (Int(arc4random()) % 10) + 1
+
 switch x {
 case 0, 1, 2, 3, 4, 5:
     print("Entre 0 y 5")
 case 6:
-    fallthrough //  si entra en la condicion del case 6 no lo evalua y salta al siguente 'case', y pasaria al case 7!
+    print("Encontré un 6")
+    fallthrough //  si entra en la condicion del 'case 6:' no lo evalua y salta al siguente 'case', y pasaria al case 7.
 case 7:
     print("6 o 7")
 default:
     print("Mayor que 7")
 }
+
+
+///**ETIQUETAS EN LOS BUCLES**
+var valor2:Int = -1
+var intentos = 0
+let acierto2 = Int(arc4random()) % 20
+print("Buscando el numero \(acierto2)")
+
+busqueda: while acierto2 != valor2 { // Busqueda seria una etiqueta. NIvel 1
+    valor2 = Int(arc4random()) % 20
+    buscar: for indices in 0...19 {
+        if acierto2 != valor2 {
+            intentos += 1
+            continue busqueda // Al poner etiquetas este bucle empezaria por el de arriba.Nivel 2
+        } else {
+            break buscar // Esto te saca de este bucle que tiene el nombre de etiqueta buscar pero se sigue evaluando con el primer bucle de busqueda.Nivel 1
+        }
+    }
+    print("Evaluamos nuevo caso")
+}
+print("Enhorabuena. Salió el \(valor2) al intento número \(intentos)")
 
